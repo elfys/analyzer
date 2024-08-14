cbefca860eaa380f6cd012dd46868e6d5e36c29d
Daria Kriukova <daria.kriukova@elfys.fi>
2024-08-14 15:54:37 +0300

    fix incorrect measurements sorting

--- analyzer/tests/test_get_iv_measurements.py
+++ analyzer/tests/test_get_iv_measurements.py
@@ -10,23 +10,25 @@ from orm import (
 class TestGetIvMeasurements:
     @pytest.fixture
     def sample_conditions(self):
-        low_amplitude_measurements = [
+        low_amplitude_measurements = lambda: [
             IVMeasurement(voltage_input=-0.01, cathode_current=1),
             IVMeasurement(voltage_input=0.00, cathode_current=2),
             IVMeasurement(voltage_input=0.01, cathode_current=3)
         ]
-        high_amplitude_measurements = [
+        high_amplitude_measurements = lambda: [
             IVMeasurement(voltage_input=-1, cathode_current=40),
             IVMeasurement(voltage_input=0, cathode_current=50),
             IVMeasurement(voltage_input=1, cathode_current=60)
         ]
         conditions = [
             IvConditions(
-                id=1, measurements=low_amplitude_measurements, chip_id=1, datetime="2021-01-01"),
+                id=1, measurements=low_amplitude_measurements(), chip_id=1, datetime="2021-01-01"),
             IvConditions(
-                id=2, measurements=high_amplitude_measurements, chip_id=1, datetime="2021-01-02"),
+                id=2, measurements=high_amplitude_measurements(), chip_id=1, datetime="2021-01-01"),
             IvConditions(
-                id=3, measurements=low_amplitude_measurements, chip_id=1, datetime="2021-01-03"),
+                id=3, measurements=low_amplitude_measurements(), chip_id=1, datetime="2024-01-01"),
+            IvConditions(
+                id=4, measurements=high_amplitude_measurements(), chip_id=1, datetime="2024-01-01"),
         ]
         return conditions
     
@@ -43,13 +45,23 @@ class TestGetIvMeasurements:
         assert len(results) == 0, "Should handle empty conditions without error"
     
     @pytest.mark.parametrize("reverse", [False, True], ids=["normal", "reversed"])
-    def test_get_measurements_ordering(self, sample_conditions, reverse):
+    def test_get_measurements_returns_lowest_amplitude_for_intersect(self, sample_conditions, reverse):
         if reverse:
             sample_conditions = list(reversed(sample_conditions))
         results = get_iv_measurements(sample_conditions)
-        zero_measurement = [m for m in results if
+        zero_measurements = [m for m in results if
                             m.conditions.chip_id == 1 and m.voltage_input == 0]
-        assert len(zero_measurement) == 1, "Should have one measurement with voltage 0"
-        assert zero_measurement[0].cathode_current == 2, \
-            "Should have the measurement with low amplitude"
-        assert zero_measurement[0].conditions.datetime == "2021-01-03", "Should sort by datetime"
+        assert len(zero_measurements) == 1, "Should have one measurement with voltage 0"
+        assert zero_measurements[0].cathode_current == 2, \
+            "Should return measurement with lowest amplitude"
+    
+    @pytest.mark.parametrize("reverse", [False, True], ids=["normal", "reversed"])
+    def test_get_measurements_returns_latest(self, sample_conditions, reverse):
+        if reverse:
+            sample_conditions = list(reversed(sample_conditions))
+        results = get_iv_measurements(sample_conditions)
+        
+        assert len(results) == 5, ("Wrong number of measurements for two sweeps of 3 measurement "
+                                   "where both with one measurement for 0")
+        for measurement in results:
+            assert measurement.conditions.datetime == "2024-01-01", "Should return latest measurements"
